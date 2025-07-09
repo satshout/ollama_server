fb70cae0228f163e6e18f2407bc1e6191fc53707
satshout <128780336+satshout@users.noreply.github.com>
2025-07-09 20:56:14 +0900

    Fix: minor bugs on up.sh

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # ollama_server
+
 ollama + webui + cloudflared
 
-## When cloning your project,
+## When cloning this project,
+
 after you've cloned the main repository, you'll need additional commands to get the contents of its submodules.
 
 ```bash
 git clone https://github.com/satshout/ollama_server.git
 cd ollama_server
 git submodule update --init --recursive
+```
+
+## How to compose up
+
+```bash
+cd ollama_server
+sudo bash up.sh
+```
+
+## How to compose down
+
+```bash
+cd ollama_server
+sudo bash down.sh
 ```
--- up.sh
+++ up.sh
@@ -7,56 +7,75 @@ pDify="80"
 
 set -eux
 
-# Start ollama-webui
+# Start ollama-webui ------------------------------------
 cd ./ollama_webui
+
 docker compose pull
 docker compose up -d
+
 # Wait for the Ollama to start
 echo "Waiting for Ollama to start..."
+set +eux
 while ! curl -s http://localhost:$pOllama/ > /dev/null; do
     sleep 1
 done
+set -eux
 echo "Ollama is up and running at http://localhost:$pOllama"
+
 # Wait for Open WebUI to start
 echo "Waiting for Open WebUI to start..."
+set +eux
 while ! curl -s http://localhost:$pOpenWebUI/ > /dev/null; do
     sleep 1
 done
+set -eux
 echo "Open WebUI is up and running at http://localhost:$pOpenWebUI"
+
 cd ../
 
-# Start Dify
+# Start Dify ------------------------------------
 cd ./dify/docker
+
 git reset --hard origin/main
 git pull origin main
+
 docker compose pull
 cp .env.example .env
 docker compose up -d
+
 # Wait for Dify to start
 echo "Waiting for Dify to start..."
+set +eux
 while ! curl -s http://localhost:$pDify/ > /dev/null; do
     sleep 1
 done
+set -eux
 echo "Dify is up and running at http://localhost:$pDify"
+
 cd ../../
 
-# Start Cloudflare Tunnel
+# Start Cloudflare Tunnel ------------------------------------
 cd ./cloudflare-tunnel
+
 docker compose pull
 docker compose up -d
+
 # Display the URLs for the services
 echo "The following URLs are available:"
 
 # Wait for Cloudflare Tunnel to start
+set +eux
 urls=()
 while [ ${#urls[@]} -eq 0 ]; do
-    mapfile -t urls < <(docker compose logs | grep -Eo 'https://[a-zA-Z0-9.-]+' | sort -u)
-    sleep 1
+    mapfile -t urls < <(docker compose logs | grep -Eo 'https://[a-zA-Z0-9.-]+\.trycloudflared\.com' | sort -u)
+    sleep 5
 done
+set -eux
 
 for url in "${urls[@]}"; do
     echo "$url"
 done
+
 cd ../
 
 echo "$urls" > ./urls.txt
